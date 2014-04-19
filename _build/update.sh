30dcad5ce8a43511f9827dc42c6e1228baf4eb90
kodmunki <kodmunki@gmail.com>
2014-04-19 12:21:54 -0500

    Update update script

--- _build/update.sh
+++ _build/update.sh
@@ -4,6 +4,7 @@ echo "Copying resources to dependent projects/"
 cp -f ../bin/ku4jQuery-kernel.js ../../ku4jQuery-data/tests/_dependencies/
 cp -f ../bin/ku4jQuery-kernel.js ../../ku4jQuery-webApp/tests/_dependencies/
 cp -f ../bin/ku4jQuery-kernel.js ../../ku4jQuery-webApp/example/scripts/example/lib/
+cp -f ../bin/ku4jQuery-kernel.js ../../ku4jQuery-webApp/_TEMPLATE/lib/
 cp -f ../bin/ku4jQuery-kernel.js ../../ku4jQuery-widget/tests/_dependencies/
 
 echo "Update complete :{)}"
