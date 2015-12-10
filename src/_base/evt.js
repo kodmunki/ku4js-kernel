5ae0c052771991763c1d97706c5ed388e3a1886f
kodmunki <kodmunki@gmail.com>
2015-12-10 00:11:08 -0600

    Fix event

--- src/_base/evt.js
+++ src/_base/evt.js
@@ -1,8 +1,9 @@
 if(!$.exists($.evt)) $.evt = { };
 $.evt.mute = function(event) {
-    if(!$.exists(event)) return;
+    if(!$.exists(event)) return false;
     if($.isFunction(event.preventDefault())) event.preventDefault();
     if($.isFunction(event.stopPropagation())) event.stopPropagation();
     if($.isFunction(event.stopImmediatePropagation())) event.stopImmediatePropagation();
     event.cancelBubble = true;
+    return false;
 };
